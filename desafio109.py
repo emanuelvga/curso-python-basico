5561eca8fb1cedc500fa74961dbb950ab024e874
Emannuel Pereira de Oliveira <emanuelvga2016@gmail.com>
2022-12-24 09:36:41 -0300

    Alteração do arquivo moeda e a resolução do exercicio 109

---
+++ desafio109.py
@@ -0,0 +1,8 @@
+import moeda
+
+
+n = float(input('digite o preço: '))
+print(f'O dobro de {moeda.moeda(n)} é {moeda.dobro(n, True)}')
+print(f'A metade de {moeda.moeda(n)} é {moeda.metade(n, True)}')
+print(f'Aumentando 10% é {moeda.aumentar(n, 10, True)}')
+print(f'Diminuindo 13% é {moeda.diminuir(n, 13, True)}')
